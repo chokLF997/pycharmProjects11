0959ae18375aa2d6a67dfe36fac9ad7182560321
chokLF1 <chokLF@outlook.com>
2022-08-02 23:00:43 +0800

    Default Changelist

--- pytest_demo/test_demo/test_01.py
+++ pytest_demo/test_demo/test_01.py
@@ -2,9 +2,21 @@ import pytest
 import requests
 
 
-def test01():
-    print("hhhh")
+class Test:
+    def test_login(self):
+        url = "http://121.41.14.39:9097/api/loginS"
+        data = {
+            "username": "20154084",
+            "password": "e10adc3949ba59abbe56e057f20f883e"
+        }
+        header = {
+            "Content-Type": "application/json"
+        }
+        re = requests.post(url=url, data=data, headers=header)
+        print(re.text)
 
 
 if __name__ == '__main__':
-    pytest.main(["-v", "-s"])
+    pytest.main()
+#     # os.system("allure generate report -o ./allurereport --clean")
+#     # ['-s', '-v', '--alluredir=report']
